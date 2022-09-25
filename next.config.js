1424c4829b81b163b09fe058e30abb54f3f8843d
ekusiadadus <ekusiadadus@gmail.com>
2022-09-25 23:46:09 +0900

    feat: :sparkles: JSON server

--- next.config.js
+++ next.config.js
@@ -1,9 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  }
+  compiler: (() => {
+    let compilerConfig = {
+      // styledComponentsの有効化
+      styledComponents: true,
+    }
+
+    if (process.env.NODE_ENV === 'production') {
+      compilerConfig = {
+        ...compilerConfig,
+        // 本番環境ではReact Testing Libraryで使用するdata-testid属性を削除
+        reactRemoveProperties: { properties: ['^data-testid$'] },
+      }
+    }
+
+    return compilerConfig
+  })(),
 }
 
 module.exports = nextConfig
